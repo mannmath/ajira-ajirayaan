989f1b7cbcd215b8633b13b6d600bad52d3d4ab9
mannmath <mannmath180@gmail.com>
2021-01-10 00:03:52 +0530

    full integration testing

--- src/main/java/com/ajira/ajirayaan/person/EmployeeController.java
+++ src/main/java/com/ajira/ajirayaan/person/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.ajira.ajirayaan.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +20,10 @@ public class EmployeeController {
 //		EmployeeEntity employee = employeeService.getEmployeeByName("alex");
 //		return new ResponseEntity(employee, HttpStatus.OK);
 		return employeeService.getEmployeeByName("alex");
+	}
+
+	@GetMapping("/employee/entity")
+	public ResponseEntity<EmployeeEntity> getEmployeeEntity() {
+		return new ResponseEntity<EmployeeEntity>(employeeService.getEmployeeByName("alex"), HttpStatus.OK);
 	}
 }
--- src/test/java/com/ajira/ajirayaan/ajiraajirayaan/AjiraAjirayaanApplicationTests.java
+++ src/test/java/com/ajira/ajirayaan/ajiraajirayaan/AjiraAjirayaanApplicationTests.java
@@ -1,13 +1,41 @@
 package com.ajira.ajirayaan.ajiraajirayaan;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class AjiraAjirayaanApplicationTests {
+import com.ajira.ajirayaan.person.EmployeeEntity;
 
-	@Test
-	void contextLoads() {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AjiraAjirayaanApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
 	}
 
+	TestRestTemplate restTemplate = new TestRestTemplate();
+	HttpHeaders httpHeaders = new HttpHeaders();
+
+	@Test
+	public void givenUserName_whenUserName_thenReturnEmployee() throws Exception {
+		HttpEntity<EmployeeEntity> entity = new HttpEntity<EmployeeEntity>(null, httpHeaders);
+		ResponseEntity<EmployeeEntity> response = restTemplate.exchange(createURLWithPort("/api/employee/entity"),
+				HttpMethod.GET, entity, EmployeeEntity.class);
+
+		String actual = response.getBody().getName();
+		assertEquals("alex", actual);
+	}
 }
